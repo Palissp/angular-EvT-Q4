e63d24979fa4775a6ddc2d934f91498fa44b1c86
KEVIN ALEXANDER MAXI JARA <kmaxi@pichincha.com>
2022-12-16 11:07:03 -0500

    Add overall happy layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import { RegisterFormComponent } from './register/register-form/register-form.component';
+import { RegisterViewComponent } from './register/register-view/register-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterFormComponent
+    RegisterFormComponent,
+    RegisterViewComponent
   ],
   imports: [
     BrowserModule,
