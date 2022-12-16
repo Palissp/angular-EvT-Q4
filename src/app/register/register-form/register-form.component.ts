b10b1258572875ea998d1578d92a520cfcad8160
KEVIN ALEXANDER MAXI JARA <kmaxi@pichincha.com>
2022-12-16 11:12:42 -0500

    Set initial formbuilder

--- src/app/register/register-form/register-form.component.ts
+++ src/app/register/register-form/register-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register-form',
@@ -7,7 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterFormComponent implements OnInit {
 
-  constructor() { }
+  public registerForm: FormGroup;
+
+  constructor(
+    private formbuilder: FormBuilder,
+  ) {
+    this.registerForm = this.formbuilder.group({
+      name: ['', [Validators.required, Validators.min(4), Validators.max(40)],],
+      surname: ['', [Validators.required, Validators.min(4), Validators.max(40)],],
+      email: ['', [Validators.email]],
+      telephone: ['', [Validators.required]],
+      state: ['', [Validators.required]],
+      city: ['', [Validators.required]],
+      information: ['', [Validators.required]],
+      terms: ['', [Validators.required]],
+      save: ['false'],
+    });
+  }
 
   ngOnInit(): void {
   }
